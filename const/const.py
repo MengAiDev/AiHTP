fa17ef0be6ade01801c0eaf4cf67eaef1e3b1dd9
MengAiDev <3463526515@qq.com>
2025-03-15 23:32:25 +0000

    update const

--- const/const.py
+++ const/const.py
@@ -1,3 +1,2 @@
-KIMI_API_KEY: str = "your api key"
-EXAMPLE_CALLKIMI_FOLDER_PATH = "./test_kimi_folders"
-EXAMPLE_CALLKIMI_USER_QUESTION = "What do these files say?"
+KIMI_API_KEY     = "your api key"
+DEEPSEEK_API_KEY = "your api key"
