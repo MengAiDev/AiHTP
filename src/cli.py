8a09a1b76acc0024a4586e1919d2a034b075015b
MengAiDev <3463526515@qq.com>
2025-03-21 11:00:07 +0000

    update cli

--- src/cli.py
+++ src/cli.py
@@ -8,3 +8,10 @@ file_paths = list_files_in_folder("../kimi_knowledge") + [htp_image_path]
 
 htp_image_reader = kimi.KimiVisionClient(const.KIMI_API_KEY)
 
+print("please wait for kimi's reply...")
+
+messages = htp_image_reader.upload_files(file_paths)
+kimi_answer = htp_image_reader.ask_vision_model(messages, "There is a htp test image. And there are also some papers. What can you get?")
+
+print("kimi's reply", kimi_answer)
+
